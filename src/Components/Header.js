5c3f6d63f68ec3c9d779ad52884b5df50f1c1e40
3psolucoesprojetos <3psolucoesprojetos@gmail.com>
2023-07-17 22:27:29 -0300

    commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Header extends React.Component {
+    render()
+    {
+        return <h2 align="center">Integra Contador</h2>
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Components/Header.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import Footer from './Components/Footer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Header />
+        <App />
+
+      <Footer />
   </React.StrictMode>
 );
 
